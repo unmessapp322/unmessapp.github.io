26b5b06377a46ac284eba0de5b3246a4f7592acb
biraj silwal <bsilwal@cs.unm.edu>
2020-07-27 20:12:25 -0600

    dfdf

---
+++ precache-manifest.73304a972c87b3da65e27d2a2dc267ca.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e327092e2c265ace6f879e0bbae469bc",
+    "url": "/index.html"
+  },
+  {
+    "revision": "756e6ac0d3053062dda3",
+    "url": "/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "142ca0d4fc1d466bd517",
+    "url": "/static/css/main.eddb3cf7.chunk.css"
+  },
+  {
+    "revision": "756e6ac0d3053062dda3",
+    "url": "/static/js/2.06ee8f8f.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/static/js/2.06ee8f8f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "142ca0d4fc1d466bd517",
+    "url": "/static/js/main.74704c23.chunk.js"
+  },
+  {
+    "revision": "48f6615f652d4d3d047d",
+    "url": "/static/js/runtime-main.cd354ea7.js"
+  },
+  {
+    "revision": "a0b2f2e25b5d3b87befc365c9d6a47af",
+    "url": "/static/media/AuctionHouse.a0b2f2e2.png"
+  },
+  {
+    "revision": "7d1cc921d376d3fe6a6a4b29fecc674a",
+    "url": "/static/media/applogo.7d1cc921.png"
+  },
+  {
+    "revision": "b58f185b641b257ea05ed2d378ab85cc",
+    "url": "/static/media/flixster.b58f185b.png"
+  },
+  {
+    "revision": "16bea947d2c6f8c2bacfdf5578c8ff45",
+    "url": "/static/media/myLobo.16bea947.png"
+  },
+  {
+    "revision": "4e6df71628ddf8bc257ad31b1cb6a10d",
+    "url": "/static/media/profilePic.4e6df716.png"
+  },
+  {
+    "revision": "aefd17eb2ba5a4c7d60172b7c1988908",
+    "url": "/static/media/robot2.aefd17eb.png"
+  },
+  {
+    "revision": "f2dfd5f3c49b5616cf09c6fa4eae60aa",
+    "url": "/static/media/twitter.f2dfd5f3.png"
+  }
+]);
